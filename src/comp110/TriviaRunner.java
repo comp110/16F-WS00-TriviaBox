982bd724b9b872b327aca6c6d59ced2e623a6f2d
Kris Jordan <krisjordan@gmail.com>
2016-09-02 15:59:07 -0400

    Initial commit of WS0.

---
+++ src/comp110/TriviaRunner.java
@@ -0,0 +1,29 @@
+package comp110;
+
+public class TriviaRunner {
+
+  public static void main(String[] args) {
+
+    // TODO: 
+    // 1. Declare a variable, named anything you'd like, of type TriviaBox
+    // 2. Assign to the variable a new instance of the TriviaBox class
+
+    System.out.println("The building you'll find COMP110 office hours in is:");
+    System.out.println(); // TODO: call your instance's officeHoursBuilding method and print the value returned
+
+    System.out.println("Number of UNC Football home games:");
+    System.out.println(); // TODO: call your instance's officeHoursBuilding method and print the value returned
+
+    System.out.println("On Thursday, November 10th, Dook's score will be:");
+    System.out.println(); // TODO: call your instance's dookScore method and print the value returned
+
+    System.out.println("On Thursday, November 10th, UNC's score will be:");
+    System.out.println(); // TODO: call your instance's uncScore method and print the value returned
+
+    double fahrenheit = 85.0;
+    System.out.println(fahrenheit + " degrees fahrenheit converted to celsius is:");
+    System.out.println(); // TODO: call fahrenheitToCelsius using the fahrenheit variable as input
+
+  }
+
+}
