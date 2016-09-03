faeeba80165056bbd1ce22343a6f4deb745c40ce
Kris Jordan <krisjordan@gmail.com>
2016-09-02 23:13:41 -0400

    Update for release.

--- src/comp110/TriviaRunner.java
+++ src/comp110/TriviaRunner.java
@@ -9,20 +9,21 @@ public class TriviaRunner {
     // 2. Assign to the variable a new instance of the TriviaBox class
 
     System.out.println("The building you'll find COMP110 office hours in is:");
-    System.out.println(); // TODO: call your instance's officeHoursBuilding method and print the value returned
+    // TODO: call your instance's officeHoursBuilding method and print the value returned
 
     System.out.println("Number of UNC Football home games:");
-    System.out.println(); // TODO: call your instance's officeHoursBuilding method and print the value returned
+    // TODO: call your instance's uncScore method and print the value returned
 
     System.out.println("On Thursday, November 10th, Dook's score will be:");
-    System.out.println(); // TODO: call your instance's dookScore method and print the value returned
+    // TODO: call your instance's dookScore method and print the value returned
 
     System.out.println("On Thursday, November 10th, UNC's score will be:");
-    System.out.println(); // TODO: call your instance's uncScore method and print the value returned
+    // TODO: call your instance's uncScore method and print the value returned
 
-    double fahrenheit = 85.0;
+    double fahrenheit;
+    fahrenheit = 85.0;
     System.out.println(fahrenheit + " degrees fahrenheit converted to celsius is:");
-    System.out.println(); // TODO: call fahrenheitToCelsius using the fahrenheit variable as input
+    // TODO: call fahrenheitToCelsius using the fahrenheit variable as input
 
   }
 
